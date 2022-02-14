1a5690331a01ee76588faa8e6fefa63aa4f0e880
Matteo Manica <tte@zurich.ibm.com>
2022-02-14 19:11:06 +0100

    tests: fixing S3 tests configuration.
    Signed-off-by: Matteo Manica <tte@zurich.ibm.com>

--- src/gt4sd/conftest.py
+++ src/gt4sd/conftest.py
@@ -7,9 +7,3 @@ import pytest
 def mock_wrong_s3_env(monkeypatch):
     """Changes an environment variable to break the s3 connection."""
     monkeypatch.setenv("GT4SD_S3_SECRET_KEY", "(╯°□°）╯︵ ┻━┻")
-
-
-@pytest.fixture
-def mock_missing_s3_env(monkeypatch):
-    """Deletes an environment variable to break the s3 connection."""
-    monkeypatch.delenv("GT4SD_S3_SECRET_KEY")
--- src/gt4sd/tests/utils.py
+++ src/gt4sd/tests/utils.py
@@ -8,10 +8,10 @@ from pydantic import BaseSettings
 class GT4SDTestSettings(BaseSettings):
     """Utility variables for the tests setup."""
 
-    gt4sd_s3_host: str = "localhost:9000"
-    gt4sd_s3_access_key: str = "access-key"
-    gt4sd_s3_secret_key: str = "secret-key"
-    gt4sd_s3_secure: bool = False
+    gt4sd_s3_host: str = "s3.mil01.cloud-object-storage.appdomain.cloud"
+    gt4sd_s3_access_key: str = "a19f93a1c67949f1a31db38e58bcb7e8"
+    gt4sd_s3_secret_key: str = "5748375c761a4f09c30a68cd15e218e3b27ca3e2aebd7726"
+    gt4sd_s3_secure: bool = True
     gt4sd_ci: bool = False
 
     class Config:
