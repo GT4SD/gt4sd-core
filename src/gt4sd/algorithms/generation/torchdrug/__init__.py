4f7ebf62fc9323099579e90f6ead3432504f96db
Jannis Born <jab@zurich.ibm.com>
2022-03-10 13:17:57 +0100

    wip: stub for torchdrug integration

---
+++ src/gt4sd/algorithms/generation/torchdrug/__init__.py
@@ -0,0 +1,23 @@
+"""TorchDrugGenerationAlgorithm initialization."""
+
+from .core import (
+    TorchDrugGCPN,
+    TorchDrugZincGCPN,
+    TorchDrugQedGCPN,
+    TorchDrugPlogpGCPN,
+    TorchDrugGAF,
+    TorchDrugZincGAF,
+    TorchDrugQedAF,
+    TorchDrugPlogpAF,
+)
+
+__all__ = [
+    "TorchDrugGCPN",
+    "TorchDrugZincGCPN",
+    "TorchDrugQedGCPN",
+    "TorchDrugPlogpGCPN",
+    "TorchDrugGAF",
+    "TorchDrugZincGAF",
+    "TorchDrugQedAF",
+    "TorchDrugPlogpAF",
+]
