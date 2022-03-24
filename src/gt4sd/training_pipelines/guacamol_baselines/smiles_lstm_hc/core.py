cc93db22e61d2f8117d7c5b75b429e33cbe01f0b
Ashish Dave <Ashish13898@gmail.com>
2022-03-24 14:19:45 +0000

    feat: modified trainer class name and added saving arguments for LSTM trainer

--- src/gt4sd/training_pipelines/guacamol_baselines/smiles_lstm_hc/core.py
+++ src/gt4sd/training_pipelines/guacamol_baselines/smiles_lstm_hc/core.py
@@ -11,26 +11,29 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-class SMILESLSTMHCTrainingPipeline(GuacamolBaselinesTrainingPipeline):
+class GuacamolLSTMHCTrainingPipeline(GuacamolBaselinesTrainingPipeline):
     """SMILES LSTM HC training pipelines."""
 
     def train(  # type: ignore
         self,
+        training_args: Dict[str, Any],
         model_args: Dict[str, Any],
         dataset_args: Dict[str, Any],
     ) -> None:
-        trainer = SmilesRnnDistributionLearner(**model_args)
-        with open(dataset_args["train_smiles_filepath"]) as f:
+        params = {**training_args, **model_args, **dataset_args}
+        with open(params.pop("train_smiles_filepath")) as f:
             train_list = f.readlines()
 
-        with open(dataset_args["test_smiles_filepath"]) as f:
+        with open(params.pop("test_smiles_filepath")) as f:
             valid_list = f.readlines()
+        trainer = SmilesRnnDistributionLearner(**params)
+
         trainer.train(training_set=train_list, validation_set=valid_list)
 
 
 @dataclass
-class SMILESLSTMHCModelArguments(TrainingPipelineArguments):
-    """Arguments related to SMILES LSTM HC trainer."""
+class GuacamolLSTMHCTrainingArguments(TrainingPipelineArguments):
+    """Training Arguments related to SMILES LSTM HC trainer."""
 
     __name__ = "training_args"
 
@@ -41,18 +44,26 @@ class SMILESLSTMHCModelArguments(TrainingPipelineArguments):
         default=1000, metadata={"help": "Validate every so many batches"}
     )
     n_epochs: int = field(default=10, metadata={"help": "Number of training epochs"})
-    max_len: int = field(
-        default=100, metadata={"help": "Max length of a SMILES string"}
-    )
     hidden_size: int = field(default=512, metadata={"help": "Size of hidden layer"})
     n_layers: int = field(default=3, metadata={"help": "Number of layers for training"})
     rnn_dropout: float = field(default=0.2, metadata={"help": "Dropout value for RNN"})
     lr: float = field(default=1e-3, metadata={"help": "RNN learning rate"})
+
+
+@dataclass
+class GuacamolLSTMHCModelArguments(TrainingPipelineArguments):
+    """Arguments related to SMILES LSTM HC trainer."""
+
+    __name__ = "model_args"
+
+    max_len: int = field(
+        default=100, metadata={"help": "Max length of a SMILES string"}
+    )
     output_dir: str = field(default="", metadata={"help": "Output directory"})
 
 
 @dataclass
-class SMILESLSTMHCDataArguments(TrainingPipelineArguments):
+class GuacamolLSTMHCDataArguments(TrainingPipelineArguments):
     """Arguments related to SMILES LSTM HC data loading."""
 
     __name__ = "dataset_args"
@@ -62,4 +73,15 @@ class SMILESLSTMHCDataArguments(TrainingPipelineArguments):
     )
     test_smiles_filepath: str = field(
         metadata={"help": "Path of SMILES file for Validation."}
+    )
+
+
+@dataclass
+class GuacamolLSTMHCSavingArguments(TrainingPipelineArguments):
+    """Saving arguments related to PaccMann trainer."""
+
+    __name__ = "saving_args"
+
+    model_path: str = field(
+        metadata={"help": "Path where the model artifacts are stored."}
     )
