eab84c93e63babc7ca15e51019be2dd264c6541e
Ashish Dave <Ashish13898@gmail.com>
2022-03-24 14:19:45 +0000

    feat: modified trainer class name and added saving arguments for LSTM trainer

--- src/gt4sd/training_pipelines/guacamol_baselines/smiles_lstm_ppo/core.py
+++ src/gt4sd/training_pipelines/guacamol_baselines/smiles_lstm_ppo/core.py
@@ -1,10 +1,12 @@
 import logging
 from dataclasses import dataclass, field
 from typing import Any, Dict
-from pathlib import Path
 from guacamol_baselines.smiles_lstm_ppo.ppo_trainer import PPOTrainer
-from guacamol_baselines.smiles_lstm_hc.rnn_utils import load_rnn_model
+from guacamol_baselines.smiles_lstm_hc.rnn_model import SmilesRnn
 from guacamol_baselines.smiles_lstm_ppo.rnn_model import SmilesRnnActorCritic
+from gt4sd.algorithms.conditional_generation.guacamol.implementation import (
+    get_target_parameters,
+)
 from ...core import TrainingPipelineArguments
 from ..core import GuacamolBaselinesTrainingPipeline
 
@@ -12,31 +14,37 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-class SMILESLSTMPPOTrainingPipeline(GuacamolBaselinesTrainingPipeline):
+class GuacamolLSTMPPOTrainingPipeline(GuacamolBaselinesTrainingPipeline):
     """SMILES LSTM PPO training pipelines."""
 
-    def train(self, model_args: Dict[str, Any]) -> None:  # type: ignore
-        model_def = Path(model_args["model_path"]).with_suffix(".json")
-        smiles_rnn = load_rnn_model(
-            model_def,
-            model_args["model_path"],
-            model_args["device"],
-            copy_to_cpu=True,
-        )
-        model = SmilesRnnActorCritic(smiles_rnn=smiles_rnn).to(model_args["device"])
-        model_args["model"] = model
-        trainer = PPOTrainer(**model_args)
+    def train(self, model_args: Dict[str, Any], train_args: Dict[str, Any]) -> None:  # type: ignore
+        params = {**model_args, **train_args}
+        params["optimization_objective"] = get_target_parameters(
+            params["optimization_objective"]
+        )[0][0]
+        model_params = [
+            params.pop("input_size"),
+            params.pop("input_size"),
+            params.pop("output_size"),
+            params.pop("n_layers"),
+            params.pop("rnn_dropout"),
+        ]
+        model = SmilesRnn(*model_params)
+        model.to(params["device"])
+        model = SmilesRnnActorCritic(smiles_rnn=model).to(params["device"])
+        params["model"] = model
+        trainer = PPOTrainer(**params)
         trainer.train()
 
 
 @dataclass
-class SMILESLSTMPPOModelArguments(TrainingPipelineArguments):
+class GuacamolLSTMPPOModelArguments(TrainingPipelineArguments):
     """Arguments related to SMILES LSTM PPO trainer."""
 
     __name__ = "training_args"
 
     model: str = field(metadata={"help": "Model Path."})
-    optimization_objective: str = field(
+    optimization_objective: Dict[str, Any] = field(
         metadata={"help": "Scoring Function used for training."}
     )
     max_seq_length: int = field(
@@ -46,6 +54,14 @@ class SMILESLSTMPPOModelArguments(TrainingPipelineArguments):
         default="cpu",
         metadata={"help": 'Device to run: "cpu" or "cuda:<device number>".'},
     )
+
+
+@dataclass
+class GuacamolLSTMPPOTrainingArguments(TrainingPipelineArguments):
+    """Arguments related to SMILES LSTM PPO trainer."""
+
+    __name__ = "training_args"
+
     num_epochs: int = field(
         default=10, metadata={"help": "Number of epochs to sample."}
     )
@@ -70,4 +86,35 @@ class SMILESLSTMPPOModelArguments(TrainingPipelineArguments):
     kl_div_weight: float = field(
         default=5.0,
         metadata={"help": "Used for calculating Kullback-Leibler divergence loss."},
+    )
+    input_size: int = field(
+        default=47,
+        metadata={"help": "Number of input symbols."},
+    )
+    hidden_size: int = field(
+        default=1024,
+        metadata={"help": "Number of hidden units."},
+    )
+    output_size: int = field(
+        default=47,
+        metadata={"help": "Number of output symbols."},
+    )
+    n_layers: int = field(
+        default=3,
+        metadata={"help": "Number of hidden layers."},
+    )
+    rnn_dropout: float = field(
+        default=0.2,
+        metadata={"help": "Recurrent dropout."},
+    )
+
+
+@dataclass
+class GuacamolLSTMPPOSavingArguments(TrainingPipelineArguments):
+    """Saving arguments related to PaccMann trainer."""
+
+    __name__ = "saving_args"
+
+    model_path: str = field(
+        metadata={"help": "Path where the model artifacts are stored."}
     )
