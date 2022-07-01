ccebf6645cb34d6be69668512680bce24d7dfe0e
Dimitrios Christofidellis <72087885+christofid@users.noreply.github.com>
2022-07-01 08:12:59 +0200

    Add customizable tensoboard logger (#103)
    * Add customizable tensoboard logger
    
    * Fix parameters typing
    
    * Use the proper field type
    
    * Apply style
    
    Co-authored-by: Matteo Manica <drugilsberg@gmail.com>

--- src/gt4sd/training_pipelines/__init__.py
+++ src/gt4sd/training_pipelines/__init__.py
@@ -57,6 +57,7 @@ from .pytorch_lightning.core import PytorchLightningTrainingArguments
 from .pytorch_lightning.granular.core import (
     GranularDataArguments,
     GranularModelArguments,
+    GranularPytorchLightningTrainingArguments,
     GranularSavingArguments,
     GranularTrainingPipeline,
 )
@@ -119,7 +120,7 @@ TRAINING_PIPELINE_ARGUMENTS_MAPPING = {
         TorchDrugGraphAFModelArguments,
     ),
     "granular-trainer": (
-        PytorchLightningTrainingArguments,
+        GranularPytorchLightningTrainingArguments,
         GranularDataArguments,
         GranularModelArguments,
     ),
