ced606639d14fb06cb272cbe0bd245e489e04326
Giorgio Giannone <giorgio.giannone@ibm.com>
2022-08-17 14:50:12 +0200

    minor changes

--- src/gt4sd/domains/materials/scorer.py
+++ src/gt4sd/domains/materials/scorer.py
@@ -364,7 +364,7 @@ class PropertyPredictorScorer(TargetValueScorer):
             name: name of the property to score.
             target: target score that will be used to get the distance to the score of the SMILES (not be confused with parameters["target"])
         """
-        self.scoring_function = PropertyPredictorRegistry.get_property_predictor( # type: ignore
+        self.scoring_function = PropertyPredictorRegistry.get_property_predictor(  # type: ignore
             name=name, parameters=parameters
         )
         self.target = target
