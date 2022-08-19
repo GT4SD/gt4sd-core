d431597c3aa9a7458f6db95aa2e6a5514686fd31
Giorgio Giannone <giorgio.giannone@ibm.com>
2022-08-19 12:55:04 +0200

    docs: add docs test

--- src/gt4sd/frameworks/gflownet/tests/qm9.py
+++ src/gt4sd/frameworks/gflownet/tests/qm9.py
@@ -28,7 +28,7 @@ import torch
 import torch.nn as nn
 import torch_geometric.data as gd
 from rdkit.Chem.rdchem import Mol as RDMol
-from torch import Tensor
+import torch
 from torch.utils.data import Dataset
 
 from gt4sd.frameworks.gflownet.dataloader.dataset import (
@@ -71,7 +71,13 @@ class QM9Dataset(GFlowNetDataset):
         target: str = "gap",
         properties: List[str] = PROPERTIES,
     ) -> None:
+        """Initialize QM9 dataset.
         
+        Args:
+            h5_file: path to the h5 file containing the dataset.
+            target: target property to optimize and build the reward.
+            properties: list of properties to use as features.
+        """
         super().__init__(
             h5_file=h5_file,
             target=target,
@@ -79,7 +85,18 @@ class QM9Dataset(GFlowNetDataset):
         )
 
 
-def thermometer(v: Tensor, n_bins=50, vmin=0, vmax=1) -> Tensor:
+def thermometer(v: torch.Tensor, n_bins: int = 50, vmin: int = 0, vmax: int = 1) -> torch.Tensor:
+    """Compute a thermometer reward using gap.
+    
+    Args:
+        v: tensor of values to compute the reward.
+        n_bins: number of bins to use.
+        vmin: minimum value of the range.
+        vmax: maximum value of the range.
+
+    Returns:
+        tensor of the reward.
+    """
     bins = torch.linspace(vmin, vmax, n_bins)
     gap = bins[1] - bins[0]
     return (v[..., None] - bins.reshape((1,) * v.ndim + (-1,))).clamp(
@@ -89,18 +106,25 @@ def thermometer(v: Tensor, n_bins=50, vmin=0, vmax=1) -> Tensor:
 
 # define task
 class QM9GapTask(GFlowNetTask):
-    """QM9 task compatible with gflownet
-
-    Code adapted from: https://github.com/recursionpharma/gflownet/blob/trunk/src/gflownet/tasks/qm9/qm9.py.
-    """
+    """QM9 task compatible with gflownet."""
 
     def __init__(
-        self,  # this is set to self.model
+        self,
         configuration: Dict[str, Any],
         dataset: Dataset,
         reward_model: nn.Module = None,
         wrap_model: Callable[[nn.Module], nn.Module] = None,
     ):
+        """Initialize QM9 task.
+
+        Code adapted from: https://github.com/recursionpharma/gflownet/blob/trunk/src/gflownet/tasks/qm9/qm9.py.
+        
+        Args:
+            configuration: configuration of the task.
+            dataset: dataset to use for the task.
+            reward_model: model to use for the reward.
+            wrap_model: function to wrap the model.
+        """
         super().__init__(
             configuration=configuration,
             dataset=dataset,
@@ -108,8 +132,15 @@ class QM9GapTask(GFlowNetTask):
             wrap_model=wrap_model,
         )
 
-    def flat_reward_transform(self, _y: Union[float, Tensor]) -> FlatRewards:
-        """Transforms a target quantity y (e.g. the LUMO energy in QM9) to a positive reward scalar."""
+    def flat_reward_transform(self, _y: Union[float, torch.Tensor]) -> FlatRewards:
+        """Transforms a target quantity y (e.g. the LUMO energy in QM9) to a positive reward scalar.
+        
+        Args:
+            _y: target quantity to transform.
+        
+        Returns:
+            reward scalar.
+        """
         y = np.array(_y)
         y = y.astype("float")
         if self._rtrans == "exp":
@@ -124,6 +155,14 @@ class QM9GapTask(GFlowNetTask):
         return FlatRewards(flat_r)
 
     def inverse_flat_reward_transform(self, rp):
+        """Inverse transform a reward scalar to a target quantity y (e.g. the LUMO energy in QM9).
+        
+        Args:
+            rp: reward scalar to transform.
+        
+        Returns:
+            target quantity.
+        """
         if self._rtrans == "exp":
             return -np.log(rp) * self._width + self._min
         elif self._rtrans == "unit":
@@ -132,7 +171,11 @@ class QM9GapTask(GFlowNetTask):
             return (1 - rp + (1 - self._percentile_95)) * self._width + self._min
 
     def load_task_models(self) -> Dict[str, nn.Module]:
-        """Loads the models for the task."""
+        """Loads the models for the task.
+        
+        Returns:
+            dictionary of models.
+        """
         gap_model = MXMNet(MXMNetConfig(128, 6, 5.0))
         try:
             state_dict = torch.load("/ckpt/mxmnet_gap_model.pt")
@@ -144,6 +187,14 @@ class QM9GapTask(GFlowNetTask):
         return {"model_task": gap_model}
 
     def sample_conditional_information(self, n):
+        """Sample conditional information for the task.
+        
+        Args:
+            n: number of samples to sample.
+
+        Returns:
+            dictionary of conditional information.
+        """
         beta = None
         if self.temperature_sample_dist == "gamma":
             beta = self.rng.gamma(*self.temperature_dist_params, n).astype(np.float32)
@@ -155,14 +206,30 @@ class QM9GapTask(GFlowNetTask):
         return {"beta": torch.tensor(beta), "encoding": beta_enc}
 
     def cond_info_to_reward(
-        self, cond_info: Dict[str, Tensor], _flat_reward: FlatRewards
+        self, cond_info: Dict[str, torch.Tensor], _flat_reward: FlatRewards
     ) -> RewardScalar:
+
+        """Compute the reward for a given conditional information.
+        
+        Args:
+            cond_info: dictionary of conditional information.
+            _flat_reward: flat reward.
+        Returns:
+            reward scalar.
+        """
         if isinstance(_flat_reward, list):
             flat_reward = torch.tensor(_flat_reward)
         return RewardScalar(flat_reward ** cond_info["beta"])
 
-    def compute_flat_rewards(self, mols: List[RDMol]) -> Tuple[RewardScalar, Tensor]:
-        """Computes the flat rewards for a list of molecules."""
+    def compute_flat_rewards(self, mols: List[RDMol]) -> Tuple[RewardScalar, torch.Tensor]:
+        """Computes the flat rewards for a list of molecules.
+        
+        Args:
+            mols: list of molecules.
+
+        Returns:
+            reward scalar and validity.
+        """
         graphs = [mol2graph(i) for i in mols]  # type: ignore[attr-defined]
         is_valid = torch.tensor([i is not None for i in graphs]).bool()
         if not is_valid.any():
