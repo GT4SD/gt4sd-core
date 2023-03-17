5ad4bf85fb65d5bc631ea187524a7327b7536691
christofid <gchridi@gmail.com>
2023-03-17 17:22:14 +0100

    Update parameter's metadata

--- src/gt4sd/training_pipelines/pytorch_lightning/molformer/core.py
+++ src/gt4sd/training_pipelines/pytorch_lightning/molformer/core.py
@@ -275,7 +275,7 @@ class MolformerDataArguments(TrainingPipelineArguments):
     batch_size: int = field(default=512, metadata={"help": "Batch size."})
 
     data_path: str = field(
-        default="", metadata={"help": "Pretraining - path to pubchem file."}
+        default="", metadata={"help": "Pretraining - path to the data file."}
     )
 
     max_len: int = field(default=100, metadata={"help": "Max of length of SMILES."})
