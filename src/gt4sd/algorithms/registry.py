eaebc3782263cdf8c8da7a252efe7b190bd10fc0
fiskrt <filipskogh99@hotmail.com>
2024-06-12 04:03:50 -0400

    fix: ignore mypy

--- src/gt4sd/algorithms/registry.py
+++ src/gt4sd/algorithms/registry.py
@@ -196,7 +196,7 @@ class ApplicationsRegistry:
                 ],  # type: ignore
             )
             # NOTE: Needed to circumvent a pydantic TypeError: Parameter list to Generic[...] cannot be empty
-            VanillaConfiguration.__parameters__ = (TypeVar("T"),)
+            VanillaConfiguration.__parameters__ = (TypeVar("T"),) # type: ignore
             # NOTE: Duplicate call necessary for pydantic >=1.10.* - see https://github.com/pydantic/pydantic/issues/4695
             PydanticConfiguration: Type[AlgorithmConfiguration] = dataclass(  # type: ignore
                 VanillaConfiguration
